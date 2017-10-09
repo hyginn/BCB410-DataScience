ab06f352e5b39f0fed821f48eb22c5922cc4b5ff
albscui <albscui@gmail.com>
2017-10-09 17:46:56 -0400

    Almost done with README.Rmd

---
+++ quantifying-ml-performance/learningUnit.R
@@ -0,0 +1,45 @@
+# ___ID___.R
+#
+# Purpose:  A Bioinformatics Course:
+#              R code accompanying the APB-ML-Measuring_performance unit.
+#
+# Version:  0.1
+#
+# Date:     2017  10  09
+# Author:   Aobo Cui (albert.cui@mail.utoronto.ca)
+#
+# Versions:
+#           0.1    (Describe ...)
+
+#
+# TODO:
+#
+#
+# == DO NOT SIMPLY  source()  THIS FILE! =======================================
+#
+# If there are portions you don't understand, use R's help system, Google for an
+# answer, or ask your instructor. Don't continue if you don't understand what's
+# going on. That's not how it works ...
+#
+# ==============================================================================
+
+# = 1 Section
+
+
+
+# = 1.1 Subsection
+
+
+
+
+
+
+# = 99  Task solutions
+
+
+# = 99.1  Task 1: ...
+
+# = 99.2  Task 2: ...
+
+
+# [END]
