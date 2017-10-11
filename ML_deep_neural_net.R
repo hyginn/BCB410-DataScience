5a988a0672344420ecb8b8da09fe6675478a1c36
jyjenny <jyjenny11@gmail.com>
2017-10-11 10:43:16 -0400

    initial commit - test

---
+++ ML_deep_neural_net.R
@@ -0,0 +1,45 @@
+# ___ID___.R
+#
+# Purpose:  A Bioinformatics Course:
+#              R code accompanying the ML_Deep_Neural_Networks unit.
+#
+# Version:  0.1
+#
+# Date:     2017  10  11
+# Author:   Jenny Yin (jyjenny11@gmail.com)
+#
+# Versions:
+#           0.1    (Describe ...)
+
+#
+# TODO:
+#
+#
+# == DO NOT SIMPLY  source()  THIS FILE! =======================================
+#
+# If there are portions you don't understand, use R's help system, Google for an
+# answer, or ask your instructor. Don't continue if you don't understand what's
+# going on. That's not how it works ...
+#
+# ==============================================================================
+
+# = 1 Section
+
+
+
+# = 1.1 Subsection
+
+
+
+
+
+
+# = 99  Task solutions
+
+
+# = 99.1  Task 1: ...
+
+# = 99.2  Task 2: ...
+
+
+# [END]
