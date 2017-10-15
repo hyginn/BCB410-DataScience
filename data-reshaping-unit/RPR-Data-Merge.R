c8348ee3b89fa142c4f197eb9b38f56bc53e68c8
Betty <zhangyuhanbetty@outlook.com>
2017-10-14 20:14:19 -0400

    feedback

--- data-reshaping-unit/RPR-Data-Merge.R
+++ data-reshaping-unit/RPR-Data-Merge.R
@@ -5,11 +5,11 @@
 #
 # Version:  0.1
 #
-# Date:     2017  Oct  10th
+# Date:     2017  Oct  14th
 # Author:   Yuhan Zhang(yuhan.zhang@mail.utoronto.ca)
 #
 # Versions:
-#           0.1    (Describe ...)
+#           0.2    (After feedback)
 
 #
 # TODO:
@@ -45,7 +45,11 @@
 #R and Rstudio basic concepts and functions, e.g:vector, factor and data.frame() etc.
 # Understanding basic Table,matrix, and data frame structures.
 
-# = 5.1 Section : add columns or rows prespectively
+
+
+# = 5 Section :Useful functions
+
+# = 5.1 Subsection : add columns or rows prespectively
 #adding a single column from 1 to 7
 #cbind for add column, rbind for add rows
 m <- cbind(1, 1:7)
@@ -62,15 +66,18 @@ cbind(I = 0, X = rbind(a = 1, b = 1:3))
 
 # Be careful that if you want to use rbind(data frame A, data Frame B), you need
 # to make sure that they have the same variables.
+# cbind and rbind bind cols and rows without matching common columns, which means you may insert wrong values
+# in some row/cols.
+#Therefore, we introduce other function below to merge two frames when we want to match some columns.
 
-
-# = 5.2 Section: Merge() function in R
+# = 5.2 Subsection: Merge() function in R
 #What if two frames does not have the same varibales? we use merge() to add them together.
 help(merge)
 
 #merge(x,y,..)
 #x, y are frames to be merged
-#by, by.x, by.y specification of column used for merging
+#by, specification of column you want to match on;by.x and by.y used when
+#two data frames have different names for the columns you want to match on,
 #all. all.x all.y  If True then extra row/column will be extra rows will be added to the output,
 #one for each row in x that has no matching row in y.
 #These rows will have NAs in those columns that are usually filled with values from y.
@@ -78,10 +85,14 @@ help(merge)
 
 #merge(frameA,frameB, by="ID") merged by common column ID
 #merge(frameA,frameB,by=c("ID","Country")) merged by ID and Country
+#merge(frameA,frameB,by.x = "ID",by.y = "Id")merged by ID of frameA and Id of frameB.
 
-# = 5.2.1 Task 1: merge aaTrans and nucle from Yeast GOSlim by their common columns
+#When merging directly, be careful that x,y are the same data structure. If not, you'd better
+#convert one of them to become useful.
 
-# prepare aaTrans and nucle from GOSlim of yeast
+# = 5.2.1 Convert ExpressionSet into matrix or frame.
+#simply provide a simple way:
+#prepare downloading data
 install.packages("readr")
 library(readr)
 # Load the bioconductor package installer
@@ -92,6 +103,40 @@ library(Biobase)
 
 biocLite("GEOquery")
 library(GEOquery)
+
+gset <- getGEO("GSE3635", GSEMatrix =TRUE, getGPL=FALSE)
+
+if (length(gset) > 1) {
+  idx <- grep("GPL1914", attr(gset, "names"))
+} else {
+  idx <- 1
+}
+# Fallback data - in case the GEO server is not working:
+gset <- gset[[idx]]
+GSE3635 <- gset
+save(GSE3635, file="./data/GSE3635.RData")
+load(file="./data/GSE3635.RData")
+gset <- GSE3635
+#now gset is a eset.
+m <- exprs(gset) #use exprs to return matrix of expression of values stored in m.
+#Assumed we want to pick pheoData as new rows/cols in new matrix.
+pdata <- pData(gset)
+d <- cbind(pdata, t(m))
+#use cbind of combine them. t() is tranpose function used to convert rows and columns
+# to make sure the number of rows and cols can match.
+d[1:5,1:5] #see first couple of lines
+d <- data.frame(d) # making it as frame
+# then we can merge with other frames.
+
+
+
+
+# = 5.2.2 Task 1: (test basic use of merge in bio data, be careful of multiple common column names)
+#We want to investigate difference of genes with amino acid transportation and nucleolus annotations.
+#To do that, you need:
+#merge aaTrans and nucle from Yeast GOSlim by their common columns to create a new data frame to work.
+
+# prepare aaTrans and nucle from GOSlim of yeast
 
 #Download Yeast GOSlim
 url <- "https://downloads.yeastgenome.org/curation/literature/go_slim_mapping.tab"
@@ -105,47 +150,38 @@ colNames <- c("ID",
               "termID",
               "status")
 
+na.omit(scGsl) ## remove NA values
 #split scGsl into multiple groups by its termName
 newset = split(scGsl, scGsl$termName)
 #derive aaTrans and nucle using split
 aaTrans <- newset$`amino acid transport`
 nucle <- newset$nucleolus
 
-# merge aaTrans and nucle:
+# merge aaTrans and nucle below :
 
 
-# = 6 Section : combine() for eset
+# = 5.3 Subsection : combine() for ExpressionSet(eset)
 #combine(eset1, eset2)
 
-
-# = 6.1 Task 2:
-gset <- getGEO("GSE3635", GSEMatrix =TRUE, getGPL=FALSE)
-
-if (length(gset) > 1) {
-  idx <- grep("GPL1914", attr(gset, "names"))
-} else {
-  idx <- 1
-}
-
-gset <- gset[[idx]]
-GSE3635 <- gset
-save(GSE3635, file="./data/GSE3635.RData")
-load(file="./data/GSE3635.RData")
-gset <- GSE3635
+# = 5.3.1 Task 2:
+#since gset and gset2 has common featureNames, assumed that we want to merge this two ExpressionSets
+# for 6 common featureNames(YAL001C to YAL007C) with 6 samples in gset and 6 samples in gset2
+#what whould you code for?
 
 
+#fallback data in case of not working
 GSE4987 <- getGEO("GSE4987", GSEMatrix =TRUE, AnnotGPL=TRUE)
 length(GSE4987) # 1
 GSE4987 <- GSE4987[[1]]
 save(GSE4987, file="./data/GSE4987.RData")
 load(file="./data/GSE4987.RData")
 gset2 <- GSE4987
-#since gset and gset has common featureNames, if we want to merge this two ExpressionSets
-# for 6 common featureNames(YAL001C to YAL007C) with 6 samples in gset and 6 samples in gset2
-#what whould you code for?
+#please implement codes below :
 
 
-# = 7 Section :Application in large-scale analysis
+
+
+# = 6 Section :Application in large-scale analysis
 #An increasing amount of microarray gene expression data sets is available through public repositories such as NCBI GEO.
 #The public repositories might contain the necessary clues for the discovery of new findings,
 #leading to the development of new treatments or therapies
@@ -153,6 +189,19 @@ gset2 <- GSE4987
 #Integration of this vast amount of data originating from different but independent studies
 #could be beneficial for the discovery of new biological insights
 #by increasing the statistical power of gene expression analysis.
+
+# = 7 Section : Practice for application:
+#merging and integrating data can be used after spliting and analyzing each group data,in order to combine
+#results,new features and give conclusions, saved as new data created.
+#Also,you will need merging ability when you want to create model(could be used in machine learning or regression etc.)
+#that needs information from more than one data frames/profiles.
+#For example
+#Task 3: (test whether understand and can use all functions above)
+#here some student wants to create a training model for classification to investigate GO and corresponding
+#data from microarrays, combining termID (as one column) in Yeast GOSlim and expression
+#data from GSE3635 based on their common features/IDs like YAL002C.
+#write your codes here:
+
 
 
 
@@ -165,8 +214,25 @@ tmp = gset[12:17,1:6]
 tmp2 = gset2[12:17, 1:6]
 combine(tmp, tmp2)
 
+
+# = 8.3 Task 3
+#Since gset(data of GSE3635 derived before) has different structure as scGsl(), we need convert the format first.
+frame1 <- exprs(gset)  ## get matrix expression data from gset
+names <- featureNames(gset)  ## get feature names from gset used as row/col names
+train.data <- cbind(names, t(frame1))  ## combine feature names and expression data together
+train.data <- t(train.data)  ## convert row and cols in train.data. for now feature names are rownames
+train.data <- cbind(ID = rownames(train.data), train.data)  ## insert one column of ID as common col for further merge
+rescGsl <- scGsl[,-c(3:5)] ## reduced data that removing SGDId, Ontology and termName which are useless in this case
+train.data <- data.frame(train.data) ## convert matrix to frame.
+newdata <- merge(rescGsl,train.data, by="ID") ## merge the two data frame.
+
+
+
+
 # = 9 Section Resources
 #http://www.statmethods.net/management/merging.html
 #https://www.hindawi.com/journals/isrn/2014/345106/
+#ExpressionSet convertion https://support.bioconductor.org/p/77432/
+#merge usage http://www.cookbook-r.com/Manipulating_data/Merging_data_frames/
 
 # [END]
--- data-reshaping-unit/RPR-Data-Split.R
+++ data-reshaping-unit/RPR-Data-Split.R
@@ -5,11 +5,11 @@
 #
 # Version:  0.1
 #
-# Date:     2017  Oct  10th
+# Date:     2017  Oct  14th
 # Author:   Yuhan Zhang(yuhan.zhang@mail.utoronto.ca)
 #
 # Versions:
-#           0.1    (Describe ...)
+#           0.2    (After feedback)
 
 #
 # TODO:
@@ -26,8 +26,8 @@
 # = 1 Section - Abstract
 
 #R split and modification is usually used in processing raw data frame downloaded from original database.
-#the original data frame we've got usually not proper or too complicated for our purpose and further study
-#Therefore we reduce or rearrange data set based on requirement.
+#the original data frame we've got usually too big and complicated for our purpose and further study
+#Therefore we simplify or rearrange data set based on requirement.
 # in order to make results more reliable and unbiased, we subset the data frame and do annalysis
 #seperately or apply different algorithm in different groups(split-apply-merge analysis).
 
@@ -51,8 +51,10 @@
 
 
 
-# = 5.1 Section : subset
-# The most simplist way to split data is just get subset by known indices.
+# = 5 Section : useful functions
+
+# = 5.1 Subsection: subset
+# The most simplist way to split data is just get subset by known indices or names.
 data = data.frame(x=c(1,2,3),variable = c("a","b","c"))
 subset = data[1:2, 1:2]
 
@@ -65,23 +67,82 @@ help(split)
 #x is the data frame that we want to split, and f is factor or list of factors that we split based on.
 #split(data,data$variable)
 
-# = Section 5.2.1 Task1:
+# = Section 5.2.1 Task 1:
 df <- data.frame(
   x=rnorm(25),
   y=rnorm(25),
   g=rep(factor(LETTERS[1:5]), 5)
 )
 
-#question: how to split df by g?
+#Task 1: how to split df by g? (testing basic use of split)
 #code below:
 
 
+# = Section 6 : Application
 
-# = Section 6: split-apply-merge analysis
+# = Subsection 6.1 : data frame for Modeling
+#Usually we need to build models for investigating relationships(linear or nonlinear) between two or multiple subjects
+#In this way, we need to create a data frame specific for the model from raw data set.
+#Here is a simple example:
+#We want to investigate the relationship of expression data of global transcription profile for YAL002W intron
+#along yeast cell cycle numbers for all samples from GSE3635.
+
+#To reach the goal, we need a data frame for modeling which contains all expression data from
+#GSE3635 for YAL002W.
+
+# Load series and platform data from GEO
+gset <- getGEO("GSE3635", GSEMatrix =TRUE, getGPL=FALSE)
+
+if (length(gset) > 1) {
+  idx <- grep("GPL1914", attr(gset, "names"))
+} else {
+  idx <- 1
+}
+
+gset <- gset[[idx]]
+# Fallback data - in case the GEO server is not working:
+GSE3635 <- gset
+save(GSE3635, file="./data/GSE3635.RData")
+load(file="./data/GSE3635.RData")
+gset <- GSE3635  #save to
+gset  #print gset
+
+#see features
+featureNames(gset)[1:20]
+#see samples
+sampleNames(gset)
+#from above, we saw YAL002W at row 13
+#we can subset gset:
+gset[13,]
+
+#if we do not know the exact row number,we can use the name for access:
+exp <- featureNames(gset) == "YAL002W"
+YAL002W <- gset[exp]
+exprs(YAL002W)
+#frame YAL002W would be used in modeling later.
+
+# = Subsection 6.1.1:Task 2: (test seeking specific annoatation and split it from data.)
+#Coonstructing a data frame for model of investigation of GSE4987 expression data for YAL023C along samples.
+GSE4987 <- getGEO("GSE4987", GSEMatrix =TRUE, AnnotGPL=TRUE)
+length(GSE4987) # 1
+GSE4987 <- GSE4987[[1]]
+save(GSE4987, file="./data/GSE4987.RData")
+load(file="./data/GSE4987.RData")
+gset2 <- GSE4987
+#codes implemented here:
+
+
+
+
+# = Section 6.2: split-apply-merge analysis
 # Many data analysis problem in bioinformatics involve split-apply-merge strategy,which is
 # breaking up a big problem into multiple pieces and perform operation/functions/algorithms
 #in each piece and then combine them together. for this unit, we only concern split part.
-#here is an example:
+
+#here is a simple example:
+#If researchers want to investigate genes with dfferent functions such as amino acid transport or ATPase activity
+#derived from yeast GOSlim dataset, and apply assays/functions/algorithms on each group combined with expression data(merge part not included here),
+#Because of too many functions for genes under termName in nonorder, the fastest way to get the data we need is using split().
 install.packages("readr")
 library(readr)
 # Load the bioconductor package installer
@@ -93,7 +154,7 @@ library(Biobase)
 biocLite("GEOquery")
 library(GEOquery)
 
-#Download Yeast GOSlim
+#Download Yeast GOSlim for preparation
 url <- "https://downloads.yeastgenome.org/curation/literature/go_slim_mapping.tab"
 download.file(url, destfile = "./data/go_slim_mapping.tab", method = "libcurl")
 scGsl <- read_tsv("./data/go_slim_mapping.tab")
@@ -104,70 +165,65 @@ colNames <- c("ID",
               "termName",
               "termID",
               "status")
+na.omit(scGsl) ## remove NA values
 
-#split scGsl into multiple groups by its termName
+##First, split scGsl into multiple groups by its termName annotations
 newset = split(scGsl, scGsl$termName)
-#choose one group that annotation is amino acid transport as the interest for further analysis
+##choose one group that annotation is amino acid transport as one of the interest of the analysis
 aaTrans <- newset$`amino acid transport`
-#apply some function in aaTrans
+unique(aaTrans) ## remove duplicate rows
+##Then apply assays or some function or modeling in aaTrans
+##also for ATPase:
+ATPase <- newset$`ATPase activity`
+#repeat same step as other annotations.
 
-# = Section 6.1 Task 2: split scGsl into multiple groups by "Ontology" and choose annotation P.
+##if we use subset without split like:
+sel <- scGsl$termName == "amino acid transport"
+aaTrans_gene = unique(scGsl$ID[sel])
+#we will produce the same result. But personally I prefer the split() beacuse "$" symbol would help you
+#choose from lists of termNames instead of typing by hand,reducing lots of mistakes and save more time.
+
+# = Section 6.2.1 Task 3: (test usage of split())
+#We want to investigate all rRNA genes in yeast, please build a new data frame contains
+# all rRNA genes in yeast from GOSlim for the analysis:
 
 
-# = Section 7: Train-Test strategy
+
+
+# = Section 6.3: Train-Test strategy
 # When our objective turns to prediction instead of description and sammaries of primary data, such as Maching
 # Learning that will be discussed in other unit, we typically use test data to simulate the predictive model
-# to see whether the model works fine or not.
+# to see whether the model works fine or not,called as classification model.
 #One of methods to get train and test data is using split.
-gset <- getGEO("GSE3635", GSEMatrix =TRUE, getGPL=FALSE)
 
-if (length(gset) > 1) {
-  idx <- grep("GPL1914", attr(gset, "names"))
-} else {
-  idx <- 1
-}
-
-gset <- gset[[idx]]
-GSE3635 <- gset
-save(GSE3635, file="./data/GSE3635.RData")
-load(file="./data/GSE3635.RData")
-gset <- GSE3635
-gset  #print gset
-
-# Train subset from sample "GSM81064" to "GSM81073"
+#e.g: using GSE3635 profile to build train and test data frames for classification model.
+## Train subset from first 10 samples,sample "GSM81064" to "GSM81073"
 (train = gset[,1:10])
-#TEst subset from rest sample
+## Test subset from rest samples
 (test = gset[,11:13])
 
-# = Section 7.1 Task 3:
-GSE4987 <- getGEO("GSE4987", GSEMatrix =TRUE, AnnotGPL=TRUE)
-length(GSE4987) # 1
-GSE4987 <- GSE4987[[1]]
-save(GSE4987, file="./data/GSE4987.RData")
-load(file="./data/GSE4987.RData")
-gset2 <- GSE4987
-
-#produce Train subset using data from first 40 samples and Test subset from the rest sample.
 
 
+# = 7  Task solutions
 
 
-# = 8  Task solutions
+# = 7.1  Task 1: new <- split(df,g)
 
+# = 7.2  Task 2:
+exp2 <- featureNames(gset2) == "YAL023C"
+YAL023C <- gset2[exp]
+exprs(YAL023C)
 
-# = 8.1  Task 1: new <- split(df,g)
-
-# = 8.2  Task 2:
-onto <- split(scGsl, scGsl$Ontology)
-Ponto <- onto$P
-
-# = 8.3 Task 3:
-train2 = gset2[,1:40]
-test2  = gset2[,41:50]
+# = 7.3 Task 3:
+newset2 = split(scGsl, scGsl$status) ##split by status
+rRNA <- newset2$rRNA_gene  ## choose rRNA_gene
+unique(rRNA)   ## remove duplicate rows.
 
 
 
 # = 9 Section Resources
 #https://www.rdocumentation.org/packages/base/versions/3.4.1/topics/split
 #https://medium.com/fuzz/machine-learning-classification-models-3040f71e2529
+
+
 # [END]
