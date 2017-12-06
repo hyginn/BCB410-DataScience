28cfc01de36070c9ac56ce8d85f47b665d160d9d
adriel-m <adriel@adrielm.ca>
2017-12-06 00:20:46 -0500

    move rmarkdown file. create wrapper R to open file, add improvements

---
+++ EDA-MOD-Assessment.R
@@ -0,0 +1,28 @@
+# EDA-MOD-Assessment.R
+#
+# Purpose:  Assess various regression models on various types of data.
+#
+# Version:  1.0
+#
+# Date:     2017  12  06
+# Author:   Adriel Martinez (adriel.martinez@mail.utoronto.ca)
+#
+# Versions:
+#           1.0    Just a wrapper to open the Rmarkdown file.
+#
+# License:  GPL-3  https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+#
+# == DO NOT SIMPLY  source()  THIS FILE! =======================================
+#
+# If there are portions you don't understand, use R's help system, Google for an
+# answer, or ask your instructor. Don't continue if you don't understand what's
+# going on. That's not how it works ...
+#
+# ==============================================================================
+
+# Nothing really done with this file aside from opening the Rmarkdown file :)
+
+file.edit('R/EDA-MOD-Assessment.Rmd')
+
+# [END]
