4e79d3779dbd770f3f2b0e1fd9ec1a395193611a
albscui <albscui@gmail.com>
2017-12-06 14:23:11 -0500

    Polishing, and adding references

--- APB-ML-Measuring_performance/APB-ML-Measuring_performance.R
+++ APB-ML-Measuring_performance/APB-ML-Measuring_performance.R
@@ -24,128 +24,151 @@
 # ==============================================================================
 
 
-# = 0 Load packages
-library(caret)
-library(plotROC)
+# = 0 Initial setup
+
+# = 0.1 References
+# Any code related to the caret package is based on its documentation website
+# found here (http://topepo.github.io/caret)
+
+# This is a very concise tutorial on how to use the caret package for some
+# standard metrics for classification and regression.
+# Please read the documentation accompanied in the Rmarkdown file for a full
+# understand.
+
+library(caret)    # A library for machine learning
 library(ggplot2)
-library(mlbench)  # for loading some bench marking datasets
+library(mlbench)  # For loading some bench marking datasets
+library(plotROC)  # A helper library for plotting ROC curve
 
+set.seed(42)      # Set seed for reproducibility
 
-# = 1 Metrics for classification where predictions are classes
+# = 0.2 Helper functions
 
-# = 1.1 Generating some data to play with
-set.seed(42)
-# Randomly sample from a PMF where Pr(Class1) = 0.2 and Pr(Class2) = 0.8
-true_class <- factor(sample(
-  paste0("Class", 1:2),
-  size = 1000,
-  prob = c(.2, .8),
-  replace = TRUE
-))
-true_class <- sort(true_class)
-# Sample from a beta distribution for the probabilities produced by a "model"
-class1_probs <- rbeta(sum(true_class == "Class1"), 4, 1)
-class2_probs <- rbeta(sum(true_class == "Class2"), 1, 2.5)
-test_set <- data.frame(obs = true_class,
-                       Class1 = c(class1_probs, class2_probs))
-test_set$Class2 <- 1 - test_set$Class1
-test_set$pred <-
-  factor(ifelse(test_set$Class1 >= .5, "Class1", "Class2"))
+# Reference: This function is based on the caret documentation 
+# on measuring (found here http://topepo.github.io/caret/measuring-performance.html)
+gen_data_bin_class <- function(nsamples, balance, cutoff) {
+  # This function tries to simulate the test_set of a trained binary classifier
+  # and its predictions. It can split the dataset into various proportions of
+  # class1 and class2. Then it generates the actual probabilities of both classes
+  # using a two different beta distributions.
+  # Inputs:
+  #   nsamples: the number of samples
+  #   balance: a vector of probabilities, where the first is the probability of
+  #            one class, and the second is the the prob of the second class
+  # Outputs:
 
-# Plot the distribution of Class 1 probabilities
-ggplot(test_set, aes(x = Class1)) +
-  geom_histogram(binwidth = .05) +
-  facet_wrap(~ obs) +
-  xlab("Probability of Class #1")
-
-# Compute the confusion matrix
-confusionMatrix(data = test_set$pred, reference = test_set$obs)
-
-# Or get the confusion matrix w.r.t Precision/Recall
-confusionMatrix(data = test_set$pred,
-                reference = test_set$obs,
-                mode = "prec_recall")
-
-# Generate Accuracy and Kappa statistics
-postResample(pred = test_set$pred, obs = test_set$obs)
+  # Randomly sample from a PMF where Pr(Class1) = 0.2 and Pr(Class2) = 0.8
+  true_class <- factor(sample(
+    paste0("Class", 1:2),
+    size = nsamples,
+    prob = balance,
+    replace = TRUE
+  ))
+  true_class <- sort(true_class)
+  # Sample from a beta distribution for the probabilities produced by a "model"
+  class1_probs <- rbeta(sum(true_class == "Class1"), 4, 1)
+  class2_probs <- rbeta(sum(true_class == "Class2"), 1, 2.5)
+  test_set <- data.frame(obs = true_class,
+                         Class1 = c(class1_probs, class2_probs))
+  test_set$Class2 <- 1 - test_set$Class1
+  test_set$pred <- factor(ifelse(test_set$Class1 >= cutoff, "Class1", "Class2"))
+  
+  test_set
+}
 
 # ==============================================================================
 
-# = 2 Metrics for classification where predictions are probabilities
+# = 1 Metrics for Classification
+# First generate some data for binary classifiers with 20% Class1 and 80$ Class2 imbalance
+## @knitr testSet
+test_set <- gen_data_bin_class(1000, c(0.2, 0.8), 0.5)
 
-# Compute the area under the ROC curve and the specificity and sensitivity under 
-# the 50% cutoff.
-# Note:
-#   - this function uses the first class level to define the “event” of interest.
-#     To change this, use the lev option to the function
-#   - There must be columns in the data for each of the class probabilities
-#     (named the same as the outcome’s class levels)
-twoClassSummary(test_set, lev = levels(test_set$obs))
 
-# A similar function can be used to get the analugous precision-recall values
-# and the area under the precision-recall curve:
-# This requires MLmetrics
-prSummary(test_set, lev = levels(test_set$obs))
+# Plot the distribution of Class 1 probabilities
+# Reference: Caret documentation
+ggplot(test_set, aes(x = Class1)) +
+  geom_histogram(binwidth = .05) +
+  facet_wrap(~ obs) +
+  xlab("Distribution of Probabilities for Class #1")
 
-# Compute logLoss
+# = 1.1 Accuracy and Kappa
+# Generate Accuracy and Kappa statistics
+# In the caret package, the go to method for evaluating a model's predictions
+# is the postResample(pred, obs) method, which takes in a vector of predicted values
+# and a vector of observed values
+postResample(pred = test_set$pred, obs = test_set$obs)
+
+# = 1.2 Confusion Matrix
+# In addition to postResample, you can use the confusionMatrix function from caret
+# to generate a confusion matrix. Note this is not the same as the default
+# confusion matrix in R standart library
+confusionMatrix(data = test_set$pred, reference = test_set$obs)
+
+# = 1.3 Log-Loss
+# Compute logloss
+# caret provides a function for the Log-Loss metric
 mnLogLoss(test_set, lev = levels(test_set$obs))
 
-# A number of relevant metrics
-# the overall accuracy and Kappa statistics using the predicted classes
-# the negative of the multinomial log loss (if class probabilities are available)
-# averages of the “one versus all” statistics such as sensitivity, specificity, 
+# = 1.4 AUC
+# Use ggplot2 and plotROC (an extension to ggplot2 for ROC) to plot an ROC curve
+ggplot(test_set, aes(d = obs, m = Class2)) +
+  geom_roc(n.cuts = 5) + style_roc() + 
+  geom_rocci(sig.level = .01)
+
+# AUC only works with binary classes. For this case, the twoClassSummary function
+# is helpful for computing AUC, sensitivity, and specifity scores
+# Somethings to note:
+#    - you can specify which levels to use in the "lev" parameter
+#    - so if you want to set the true class to Class2, use:
+#      twoClassSummary(test_set, lev = c("Class2", "Class1"))
+twoClassSummary(test_set, lev = levels(test_set$obs))
+
+# = 1.5 Precision/Recall
+
+# Caret supports Precision/Recall as well, with the prSummary() function
+# Note: the AUC reported is the AUC under the Precision/Recall curve, and NOT
+# the ROC curve!
+prSummary(test_set, lev = levels(test_set$obs))
+
+# Get the confusion matrix w.r.t prec_recall
+confusionMatrix(data = test_set$pred, reference = test_set$obs, mode = "prec_recall")
+
+# = 1.6 Summary
+# We went over a number of standard metrics for classification such as
+# the accuracy and Kappa statistics using the predicted classes
+# the Log-Loss metric for the predicted probabilities, the
+# precision/recall, sensitivity, and specificity metrics, as well as
 # the area under the ROC curve, etc.
+# You can view all of them using the a convenient summary function called
+# multiClassSummary()
 multiClassSummary(test_set, lev = levels(test_set$obs))
 
 # ==============================================================================
 
-# = 3 Visualizations
+# = 2 Metrics for Regression
 
-# = 3.1 ROC
-# A helper library for plotting ROCs
-# library(ggplot2)
-# library(plotROC)
-# 
-roc <- ggplot(test_set, aes(d = obs, m = Class2)) + geom_roc(n.cuts = 5)
-styledplot <- roc + style_roc()
-styledplot + geom_rocci(sig.level = .01)
-
-
-# = 3.2 Calibration Curves
-# Calibration curves can be used to characterisze how consistent the predicted
-# class probabilities are with the observed event rates.
-
-# Other functions in the gbm package, the rms package (and others) can also produce calibrartion curves.
-# The format for the function is very similar to the lift function:
-cal_obj <- calibration(Class ~ FDA + LDA + C5.0,
-                       data = lift_results,
-                       cuts = 13)
-
-# Notice the confidence intervals for the proportions inside of the subsets:
-ggplot(cal_obj)
-
-# ==============================================================================
-
-# = 4 Metrics for regression
-# First load packages and sample data
-# You can choose any dataset with continous data as labels
+# First prepare the data. We can use any dataset with continuous number. But
+# lets just use the BostonHousing dataset
+set.seed(24)
 data(BostonHousing)
-# data(longley)
 
-dataset <- BostonHousing
-
-set.seed(280)
-bh_index <- createDataPartition(dataset$medv, p = .75, list = FALSE)
-bh_tr <- dataset[bh_index, ]
-bh_te <- dataset[-bh_index, ]
-
-set.seed(7279)
+# 2.1 RMSE and R-squared
+# First split the data into training and test set 70/30 split
+train_p <- 0.70
+test_p <- 0.30
+bh_index <- createDataPartition(BostonHousing$medv, p = train_p, list = FALSE)
+bh_tr <- BostonHousing[ bh_index, ]   # The training set
+bh_te <- BostonHousing[-bh_index, ]   # The test set
+# Train a simple linear regression model using the training set
+# and use the RMSE metric as error for model fitting
 lm_fit <- train(medv ~ . + rm:lstat,
                 data = bh_tr,
-                method = "lm")
+                method = "lm",
+                metric = "RMSE")
 bh_pred <- predict(lm_fit, bh_te)
 
 lm_fit
 
-# Get the RMSE, R-squared, and MAE
+# The postResample() function is not just for classification.
+# Here, this function can get you the RMSE, R-squared, and MAE
 postResample(pred = bh_pred, obs = bh_te$medv)
