8cb440722cd652f1af7c05bdb4a7f86f3b5d8ab2
Ricardo Ramnarine <ricardo.ramnarine@mail.utoronto.ca>
2017-12-06 23:50:55 -0500

    added updated version of EDA-DR-MDS.R

--- EDA-DR-MDS.R
+++ EDA-DR-MDS.R
@@ -3,15 +3,19 @@
 # Purpose:  A Bioinformatics Course:
 #              R code accompanying the EDA-DR-MDS unit.
 #
-# Version:  0.1
+# Version:  1
 #
-# Date:     2017  10  07
+# Date:     2017  12  06
 # Author:   Ricardo Ramnarine (ricardo.ramnarine@mail.utoronto.ca)
 #
 # Versions:
 #           0.1    (Initial - skeleton template)
 #           0.2    (Added section for dependencies of unit)
-#
+#           0.3    (Added section 2: Multidimensional Scaling)
+#           0.4    (Added section 9: Multidimensional Scaling with Biological relevance)
+#           0.5    (Added section 2.2: Nonmetric Multidimensional Scaling)
+#           0.6    (Added section 9: Multidimensional Scaling with Biological relevance)
+#           1      (Updated sections according to feedback received)   
 #
 #
 #
@@ -41,39 +45,32 @@ if (!require(MASS, quietly=TRUE)) {
   library(MASS)
 }
 
-if (!require(here, quietly=TRUE)) {
-  devtools::install_github("krlmlr/here")
-  library(here)
-}
-
-
 if(!require(scatterplot3d, quietly=TRUE)) {
   install.packages("scatterplot3d")
+}
+
+if (!require(Biobase, quietly=TRUE)) {
+  biocLite("Biobase")
+  library(Biobase)
+}
+
+if (!require(parallel, quietly=TRUE)) {
+  biocLite("parallel")
+  library(parallel)
 }
 
 #Load Libraries
 library("scatterplot3d")
 library(MASS)
 
-#Load Functions
-source("EDA-DR-MDS/performMDS.R")
+source("https://bioconductor.org/biocLite.R")
 
 #Load Data
 load("./data/GSE3635.RData")
 
 
 # = 1.1 Multidimensional Scaling (MDS) Dimension Reduction techniques
-# There are 2 flavours of MDS, each of which aim to reduce dimensions to as low as possible while
-#maintaining a good stress value. More on stress values later.
-# 2. Classic/Metric
-  #If there is a metric distance between coordinates in the matrix, the MDS technique you should use is
-  #Classic/Metric.
-  #Metric:= Any pair of objects are considered metric if dist(x,y) >= 0, d(x,y) = 0 <=> x = y, d(x,y) = d(y,x),
-  # and d(x,z) <= d(x,y) + d(y,x)
-# 3. Nonmetric
-  #If there isn't a metric distance between coordinates in the matric, the MDS technique you
-  #should use is nonmetric.
-
+# Read this section on the wiki page and then come back and work through the example.
 
 #####################################################
 
@@ -90,6 +87,9 @@ euclidean_distance_matrix
 #Let's calculate the distance matrix
 euclidean_distance <- dist(euclidean_distance_matrix)
 euclidean_distance
+
+#The Following example is adapted from Lecture 8: Multidimensional scaling by Sungkyu Jung, a Professor at the University of Pittsburgh
+#which is cited here http://steipe.biochemistry.utoronto.ca/abc/students/index.php/EDA-DR-MDS#Sources
 
 #Great, now let's apply metric multidimensional scaling to this
 #R has a built in function called cmdscale that applies Classical MDS.
@@ -122,6 +122,13 @@ segments(p3$x,p3$y,p4$x,p4$y,lwd=2,col=2)
 #The columns contain [species, sex, index, FL, RW, CL, CW, and BD] in that order which represent:
 #species := species type. "B" for Blue, "O" for Orange
 #sex := gender. "M" for Male, "F" for Female
+#FL := frontal lobe size
+#RW := rear width
+#CL := carapace length
+#CW := carapace width
+#BD := body depth
+#for more information type ?crabs in the console
+
 #index :=  1 <= index <= 50 represents the index of the particular crab within each of the
 #         four groups (B + F,B + M,O + F,O + M)
 
@@ -135,35 +142,29 @@ measurements
 
 #Let's create a distance matrix using the built in function dist()
 #Notice i'm removing the species and sex columns
-d <- dist(measurements[c(-1,-2)])
+dist_mat<- dist(measurements[c(-1,-2)])
 
 #sanity check that d is of type "dist", a distance structure
-class(d)
+class(dist_mat)
 #Look at the data in d.
-d
+dist_mat
 #It is clearly not a euclidean distance matrix (it isn't a symmetrical matrix
 #with diagionals = 0), so we will apply nonmetric MDS.
 #isoMDS is a function that comes with package MASS.
 #It performs Kruskal's non-metric MDS on a euclidean distance matrix.
 #parameter k is the desired dimension for the solution.
-result <- isoMDS(d, k=2)
+result <- isoMDS(dist_mat, k=2)
 result
-#Notice the value stress of ~0.78. What is it?
-#Stress is the difference between actual and predicted distances.
-#The closer to 0 the stress value, the better the prediction.
-#In 1964, Kruskal gave guidelines of how to interpret what a good stress value was.
-#Stress Goodness-of-fit
-#0.200 poor
-#0.100 fair
-#0.050 good
-#0.025 excellent
-#0.000 perfect
 #isoMDS returns a stress percentage. Therefore, anything below 5 is good or better. i.e less than 5%
+#Notice the value stress of ~0.78. Don't remember how to interpret the result? 
+#Refer to the wiki section "How MDS Works"
+
 
 #Let's create useful labels, i.e factors that characterize which columns each crab belongs
 #to (ex. B&F, B&M, O&F, O&M)
-d <- as.data.frame(result)
-d <- d[-3]
+result_frame <- as.data.frame(result)
+#remove third column
+result_frame <- result_frame[-3]
 descriptions <- paste(measurements[,1], measurements[,2], sep="&")
 #let's create some factors so we can categorize variables in the graph
 Groups <- factor(descriptions)
@@ -171,7 +172,7 @@ Groups <- factor(descriptions)
 Groups
 
 #plot it using ggplot
-ggplot(d, aes(x=points.1, y=points.2, group=Groups)) +
+ggplot(result_frame, aes(x=points.1, y=points.2, group=Groups)) +
   geom_point(aes(shape=Groups, color=Groups))
 
 #Notice how crabs from the same group tend to cluster closely together.
@@ -192,41 +193,40 @@ set.seed(30)
 GSE <- exprs(GSE3635)
 V2 <- substr(featureNames(GSE3635), 0, 3) == 'YAR'
 V2
-V2 <- GSE[V2,]
-
+YAR <- GSE[V2,]
+YAR
 #sanity check
-dim(V2)
-class(V2)
+dim(YAR)
+class(YAR)
 #calculate distance matrix
-d <- dist(V2)
+YAR_DIST <- dist(YAR)
 
 #sanity check
-d
+YAR_DIST
 #It is clearly not a euclidean distance matrix (it isn't a symmetrical matrix
 #with diagionals = 0), so we will apply Kruskal's non-metric MDS on a euclidean distance matrix.
 #parameter k is the desired dimension for the solution.
-result_2D <- isoMDS(d, k=2)
+result_2D <- isoMDS(YAR_DIST, k=2)
 result_2D
 #our stress value is ~11.9% which is too high. Thus, to get more accurate results,
 #we must increase the number of dimensions.
 #Of course, visualizing and understanding higher than a 3 dimensional plot isn't realistic.
 #since visualizing 7 dimensions isn't very helpful, let's continue with the 3D results, out of curiousty
 
-d_2D <- as.data.frame(result_2D)
-d_2D
-v <- featureNames(GSE3635)
+YAR_2D <- as.data.frame(result_2D)
+YAR_2D
 
 #create factor with only features that start with YAR from GSE3635
 fact <- featureNames(GSE3635)[substr(featureNames(GSE3635), 0, 3) == 'YAR']
 #sanity check
 fact
 #plot it
-ggplot(d_2D, aes(x=points.1, y=points.2, group=fact)) +
+ggplot(YAR_2D, aes(x=points.1, y=points.2, group=fact)) +
   geom_point()
 #From the plot above, we can't really say much because it's cargo cult. The data isn't reliable with a high stress value
 
 #Let's also plot it in 3D
-result_3D <- isoMDS(d, k=3)
+result_3D <- isoMDS(YAR_DIST, k=3)
 result_3D
 x_3 <- result_3D$points[,1]
 y_3 <- result_3D$points[,2]
@@ -295,10 +295,10 @@ segments(p3$x,p3$y,p4$x,p4$y,lwd=2,col=2)
 
 # = 99.4  Task 4: Stressing out? Read more to find out one great way to reduce stress.
 #A good stress value is under 5%. Let's check k=6.
-temp <- isoMDS(d, k=6)
+temp <- isoMDS(YAR_DIST, k=6)
 temp
 #no good, but it turns out that 7 works.
-good_result <- isoMDS(d, k=7)
+good_result <- isoMDS(YAR_DIST, k=7)
 good_result
 
 
